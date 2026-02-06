c93118f76deccdd473b6d21cf4fef8f86f89af99
saoodktareen <saoodktareen@gmail.com>
2026-02-06 23:58:23 +0500

    Filtering Multiple Regions

--- filter_data.py
+++ filter_data.py
@@ -4,7 +4,10 @@ from load_json import load_json
 
 def filter_data(df, config:dict):
     if "region" in config:
-        df = df[df["Continent"] == config["region"]]
+        if isinstance(config["region"], list): 
+            df = df[df["Continent"].isin(config["region"])] 
+        else: 
+            df = df[df["Continent"] == config["region"]]
     if "year" in config:
         df = df[df["Year"] == config["year"]]
     df = df.dropna(subset=["GDP"])
