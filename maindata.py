e1685f4d6546ff1f96e9ad09b0357848f5deed2b
saoodktareen <saoodktareen@gmail.com>
2026-02-06 20:27:57 +0500

    Updated Main

--- maindata.py
+++ maindata.py
@@ -2,7 +2,7 @@ from load_data import load_data
 from cleaner import clean_data
 
 def main():
-    df = load_data(r"C:\Users\LENOVOI\OneDrive\Desktop\SDA Project Phase 1\SDA-Project\gdp_with_continent_filled.csv")
+    df = load_data("gdp_with_continent_filled.csv")
     cleaned_df = clean_data(df)
     print(cleaned_df)
 
