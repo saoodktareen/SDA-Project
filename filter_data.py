5d9d28b58e8640e4dbfe072b9b34796cc127e099
saoodktareen <saoodktareen@gmail.com>
2026-02-06 23:25:47 +0500

    Complete Filter Function

--- filter_data.py
+++ filter_data.py
@@ -7,20 +7,5 @@ def filter_data(df, config:dict):
         df = df[df["Continent"] == config["region"]]
     if "year" in config:
         df = df[df["Year"] == config["year"]]
+    df = df.dropna(subset=["GDP"])
     return df
-
-df = load_data("gdp_with_continent_filled.csv")
-cleaned_df = clean_data(df)
-
-
-df_long = df.melt(
-    id_vars=["Country Name", "Country Code", "Indicator Name", "Indicator Code", "Continent"],
-    var_name="Year",
-    value_name="GDP"
-)
-df_long["Year"] = df_long["Year"].astype(int)
-
-config = load_json("config.json")
-filtered = filter_data(df_long, config)
-print(filtered)
-print("Total Rows: ", len(filtered))
