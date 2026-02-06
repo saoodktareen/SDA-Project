966b577b663ccac370d4c1ab5c318aa213b7fda0
saoodktareen <saoodktareen@gmail.com>
2026-02-06 17:37:16 +0500

    Load Data Function

--- load_data.py
+++ load_data.py
@@ -3,7 +3,3 @@ import pandas as pd # Importing Panda Library
 def load_data(file_path):
     df = pd.read_csv(file_path) # df = DataFrame (Like Excel sheet)
     return df
-
-
-df = load_data(r"C:\Users\LENOVOI\OneDrive\Desktop\SDA Project Phase 1\SDA-Project\gdp_with_continent_filled.xlsx.csv") 
-print(df.head())
