7fea5755f28ff64cc9a29bb77c47d4f4703ab2a0
saoodktareen <saoodktareen@gmail.com>
2026-02-08 02:19:51 +0500

    Update Country Visualization

--- visualize_countries.py
+++ visualize_countries.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
+plt.style.use("dark_background")
 
 def visualize_countries(df, config):
     countries = config["country"]
-    operation = config["operation"].capitalize()
+    operation = config["operation"]
+
+    print("\n===== COUNTRY RESULTS =====")
 
     for country in countries:
         country_df = df[df["Country Name"] == country]
@@ -10,9 +13,18 @@ def visualize_countries(df, config):
         yearly_gdp = (
             country_df
             .groupby("Year")["GDP"]
-            .agg("sum" if config["operation"] == "sum" else "mean")
+            .agg("sum" if operation == "sum" else "mean")
             .reset_index()
         )
+
+        # ---------- TERMINAL OUTPUT ----------
+        if operation == "sum":
+            total_gdp = yearly_gdp["GDP"].sum()
+            print(f"{country} → Total GDP (all years): {total_gdp:.2f}")
+
+        elif operation == "average":
+            avg_gdp = yearly_gdp["GDP"].mean()
+            print(f"{country} → Average GDP (1940–2024): {avg_gdp:.2f}")
 
         years = yearly_gdp["Year"]
         gdp = yearly_gdp["GDP"]
@@ -20,27 +32,33 @@ def visualize_countries(df, config):
         # ---------- LINE ----------
         plt.figure(figsize=(9, 5))
         plt.plot(years, gdp, marker="o")
-        plt.title(f"{operation} GDP Trend of {country}")
+        plt.title(f"Annual GDP Trend of {country}") #{operation.capitalize()} before GDP Trend
         plt.xlabel("Year")
-        plt.ylabel(f"{operation} GDP")
+        plt.ylabel(f" GDP Value (x10¹²)") #{operation.capitalize()} before GDP Value
         plt.grid(True)
         plt.tight_layout()
+        mng = plt.get_current_fig_manager()
+        mng.window.state('zoomed')   # Windows
         plt.show()
 
         # ---------- SCATTER ----------
         plt.figure(figsize=(9, 5))
         plt.scatter(years, gdp)
-        plt.title(f"{operation} GDP Scatter Plot of {country}")
+        plt.title(f"Annual GDP Scatter Plot of {country}") #{operation.capitalize()}
         plt.xlabel("Year")
-        plt.ylabel(f"{operation} GDP")
+        plt.ylabel(f" GDP Value (x10¹²)") #{operation.capitalize()}
         plt.tight_layout()
+        mng = plt.get_current_fig_manager()
+        mng.window.state('zoomed')   # Windows
         plt.show()
 
         # ---------- HISTOGRAM ----------
         plt.figure(figsize=(8, 5))
         plt.hist(gdp, bins=12, edgecolor="black")
-        plt.title(f"{operation} GDP Distribution of {country}")
-        plt.xlabel(f"{operation} GDP")
+        plt.title(f"Annnual GDP Distribution of {country}")
+        plt.xlabel(f" GDP Value (x10¹²)")
         plt.ylabel("Frequency")
         plt.tight_layout()
+        mng = plt.get_current_fig_manager()
+        mng.window.state('zoomed')   # Windows
         plt.show()
