7ab6096536ce08841dab8e15f89d386b3d6e0973
Shaheer-Saikhani <shaheersaikhani1010@gmail.com>
2026-02-07 14:40:41 +0500

    main with transform

--- maindata.py
+++ maindata.py
@@ -3,29 +3,21 @@ from cleaner import clean_data
 from load_json import load_json
 from filter_by_region import filter_by_region
 from filter_by_country import filter_by_country
-from Process import process   
-from visual import visualize
+from Process import process
+from transform import transform_to_long
 
 def main():
     df = load_data("gdp_with_continent_filled.csv")
     cleaned_df = clean_data(df)
-
-    df_long = cleaned_df.melt(
-        id_vars=["Country Name", "Country Code", "Indicator Name", "Indicator Code", "Continent"],
-        var_name="Year",
-        value_name="GDP"
-    )
-    df_long["Year"] = df_long["Year"].astype(int)
+    df_long = transform_to_long(cleaned_df)
 
     config = load_json("config.json")
     filtered_regions = filter_by_region(df_long, config)
     print(filtered_regions)
     print("Total Rows: ", len(filtered_regions))
+
     filtered_country = filter_by_country(df_long, config)
     print(filtered_country)
-
-    # result = process(filtered, config)  
-    # visualize(filtered, config, result)
 
 if __name__ == "__main__":
     main()
