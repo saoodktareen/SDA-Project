e7d421b240583e300a8a660dc4cc2f64a00d1ef6
Shaheer-Saikhani <shaheersaikhani1010@gmail.com>
2026-02-07 16:58:53 +0500

    updated main with validate json

--- maindata.py
+++ maindata.py
@@ -1,23 +1,35 @@
 from load_data import load_data
 from cleaner import clean_data
 from load_json import load_json
+from validate_json import validate_json
 from filter_by_region import filter_by_region
 from filter_by_country import filter_by_country
 from Process import process
 from transform import transform_to_long
 
+
 def main():
     df = load_data("gdp_with_continent_filled.csv")
     cleaned_df = clean_data(df)
-    df_long = transform_to_long(cleaned_df)
 
     config = load_json("config.json")
-    filtered_regions = filter_by_region(df_long, config)
-    print(filtered_regions)
-    print("Total Rows: ", len(filtered_regions))
+    validated_config = validate_json(config, cleaned_df)
 
-    filtered_country = filter_by_country(df_long, config)
+    if validated_config is None:
+        print("Execution stopped due to invalid JSON configuration.")
+        return
+
+    df_long = transform_to_long(cleaned_df)
+
+    filtered_regions = filter_by_region(df_long, validated_config)
+    print(filtered_regions)
+    print("Total Rows:", len(filtered_regions))
+
+    filtered_country = filter_by_country(df_long, validated_config)
     print(filtered_country)
+
+    process(filtered_country, validated_config)
+
 
 if __name__ == "__main__":
     main()
