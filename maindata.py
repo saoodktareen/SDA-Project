82aa0854c706205ecff799f7df0c387ac7d7072e
saoodktareen <saoodktareen@gmail.com>
2026-02-07 14:00:19 +0500

    Main Implementation (Without Visualization)

--- maindata.py
+++ maindata.py
@@ -1,13 +1,13 @@
 from load_data import load_data
 from cleaner import clean_data
 from load_json import load_json
-from filter_data import filter_data
+from filter_by_region import filter_by_region
+from filter_by_country import filter_by_country
 from Process import process   
 from visual import visualize
 
 def main():
-        
-    df = load_data("gdp_with_continent_filled.csv")
+    df = load_data("Test_File.csv")
     cleaned_df = clean_data(df)
 
     df_long = df.melt(
@@ -18,12 +18,14 @@ def main():
     df_long["Year"] = df_long["Year"].astype(int)
 
     config = load_json("config.json")
-    filtered = filter_data(df_long, config)
-    print(filtered)
-    print("Total Rows: ", len(filtered))
+    filtered_regions = filter_by_region(df_long, config)
+    print(filtered_regions)
+    print("Total Rows: ", len(filtered_regions))
+    filtered_country = filter_by_country(df_long, config)
+    print(filtered_country)
 
-    result = process(filtered, config)  
-    visualize(filtered, config, result)
+    # result = process(filtered, config)  
+    # visualize(filtered, config, result)
 
 if __name__ == "__main__":
     main()
